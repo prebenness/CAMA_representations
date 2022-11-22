Fix error in KL-divergence calc
- Corrects error in calculation step of KL divergence loss
- Tracks KL loss and reconstruction loss separately

toy_vae/main.py
@@ -23,22 +23,26 @@ def train(m, data, epochs=20):
   opt = torch.optim.Adam(m.parameters())
 
   for epoch in range(epochs):
-    tot_loss = 0
+    num_samples = 0
+    tot_rec_loss, tot_kl_loss = 0, 0
     for x, _ in data:
       x = x.to(device)
       opt.zero_grad()
 
       x_rec = m(x)
       
-      loss = ( (x - x_rec) ** 2 ).mean() + m.encoder.kl
+      rec_loss = ( (x - x_rec) ** 2 ).sum()
+      kl_loss = m.encoder.kl
+      loss = rec_loss + kl_loss
+      
       loss.backward()
       opt.step()
 
-      tot_loss += loss.detach().clone()
+      tot_rec_loss += rec_loss.detach().clone()
+      tot_kl_loss += kl_loss.detach().clone()
+      num_samples += x.shape[0]
 
-
-    av_loss = tot_loss / ( len(data) * data.batch_size )
-    print(f'Epoch {epoch + 1} completed - average loss: {av_loss}')
+    print(f'Epoch {epoch + 1} - rec_loss: {tot_rec_loss / num_samples} - kl_loss: {tot_kl_loss / num_samples}')
 
   return m
 
@@ -73,7 +77,7 @@ def main():
   summary(ae, tuple(data_shape))
 
   # Train model
-  ae = train(ae, data, epochs=20)
+  ae = train(ae, data, epochs=40)
 
   # Plot latent space
   plot_latent(ae, data, num_batches=100)

toy_vae/models.py
@@ -11,12 +11,13 @@ class VariationalEncoder(nn.Module):
 
     self.in_dims = in_dims
     self.latent_dim = latent_dim
-
     self.hdim = hdim
+
     self.linear1 = nn.Linear(in_features=reduce(lambda a, b: a * b, self.in_dims), out_features=self.hdim)
     self.linear2a = nn.Linear(in_features=self.hdim, out_features=self.latent_dim)
     self.linear2b = nn.Linear(in_features=self.hdim, out_features=self.latent_dim)
 
+    # Sample from Normal on GPU
     self.N = torch.distributions.Normal(0, 1)
     self.N.loc = self.N.loc.cuda()
     self.N.scale = self.N.scale.cuda()
@@ -32,7 +33,12 @@ class VariationalEncoder(nn.Module):
 
     z = mu + sigma * self.N.sample(mu.shape)
 
-    self.kl = (sigma ** 2 + mu ** 2 - torch.log(sigma) - 1/2).sum()
+    self.kl = (
+      ( sigma ** 2 ) / 2 + 
+      ( mu ** 2 ) / 2 - 
+      torch.log(sigma) - 
+      1 / 2
+    ).sum()
 
     return z
 