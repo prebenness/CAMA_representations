1d79ec80b3bb540040238640743820bcc112886a
prebenness <preben.monteiro@gmail.com>
2023-02-01 18:32:15 +0100

    Add model checkpointing and evaluation
    -- Checkpoints model every 5 epochs
    -- Computes metrics on validation dataset and stores best performing model
    -- Evaluates model on clean and perturbed test data after training

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -10,20 +10,25 @@ from torchvision.utils import save_image
 
 from src.scripts.eval_performance import eval_model
 import src.utils.config as cfg
+from src.utils.model import save_model
 
 
-def store_recons(x, x_rec, epoch):
+def store_recons(x, x_rec, tag='sample'):
     '''
     Store reconstruction of random image
     '''
-    x = x[32].detach().cpu()
-    x_rec = x_rec[32].detach().cpu()
+    x = x[0].detach().cpu()
+    x_rec = x_rec[0].detach().cpu()
 
-    out_path = os.path.join('logs', f'epoch-{epoch}-img.png')
-    save_image([x, x_rec], out_path)
+    out_dir = os.path.join(cfg.OUT_DIR, 'recons')
+    os.makedirs(out_dir, exist_ok=True)
+
+    out_file = os.path.join(out_dir, f'{tag}_image.png')
+    save_image([x, x_rec], out_file)
 
 
-def train_model(model, clean_data, pert_data):
+def train_model(model, train_loader, val_loader,
+                train_loader_pert, val_loader_pert):
     '''
     Train a DeepCAMA model using clean and perturbed data
     '''
@@ -31,12 +36,13 @@ def train_model(model, clean_data, pert_data):
 
     for epoch in range(cfg.NUM_EPOCHS if not cfg.DEBUG else 1):
 
-        # Track losses
+        # Track losses and metrics
+        best_val_acc = float('-inf')
         num_samples_clean, num_samples_pert = 0, 0
         tot_rec_loss_clean, tot_kl_loss_clean, \
             tot_rec_loss_pert, tot_kl_loss_pert = 0, 0, 0, 0
 
-        for (x_clean, y_clean), (x_pert, y_pert) in zip(clean_data, pert_data):
+        for (x_clean, y_clean), (x_pert, y_pert) in zip(train_loader, train_loader_pert):
             # Format data and send to GPU
             x_clean = x_clean.to(cfg.DEVICE)
             y_clean = y_clean.to(cfg.DEVICE)
@@ -89,11 +95,19 @@ def train_model(model, clean_data, pert_data):
                        (1 - cfg.LAMBDA) * num_samples_pert)
 
         # Check predictive power of model
+        xe, nce, acc = eval_model(model, train_loader, verbose=False)
+        val_xe, val_nce, val_acc = eval_model(model, val_loader, verbose=False)
+        val_xe_pert, val_nce_pert, val_acc_pert = eval_model(
+            model, val_loader_pert, verbose=False)
 
-        xe, nce, acc = eval_model(model, clean_data, verbose=False)
+        # If best so far, store model
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            save_model(model, tag='best')
 
-        result_text = ' '.join(f'''\
-            Epoch {epoch + 1} \
+        # Print performance to stdout
+        train_result_text = ' '.join(f'''\
+            TRAIN: Epoch {epoch + 1} \
             loss: {tot_loss / tot_samples:.3f} \
             rec_cl: {tot_rec_loss_clean / num_samples_clean:.3f} \
             kl_cl: {tot_kl_loss_clean / num_samples_clean:.3f} \
@@ -101,12 +115,26 @@ def train_model(model, clean_data, pert_data):
             kl_pt: {tot_kl_loss_pert / num_samples_pert:.3f} \
             XE: {xe:.3f} \
             NCE: {nce:.3f} \
-            acc: {acc:.3f}'''.split())
+            acc: {acc:.3f}
+            '''.split())
 
-        print(result_text)
+        val_result_text = ' '.join(f'''
+            VAL: Clean XE: {val_xe:.3f} \
+            Clean NCE: {val_nce:.3f} \
+            Clean Acc: {val_acc:.3f} \
+            Pert. XE: {val_xe_pert:.3f} \
+            Pert. NCE: {val_nce_pert:.3f} \
+            Pert. acc: {val_acc_pert:.3f} 
+            '''.split())
 
-        # DEBUG reconstruction
-        # Display some random images
-        store_recons(x_clean, x_rec_clean, epoch=epoch+1)
+        print(train_result_text)
+        print(val_result_text)
+
+        # Store a random reconstructed image for qualitative performance
+        store_recons(x_clean, x_rec_clean, tag=f'epoch={epoch+1}')
+
+        # Store checkpointed models
+        if (epoch + 1) % 5 == 0:
+            save_model(model, tag=f'epoch={epoch+1}')
 
     return model
